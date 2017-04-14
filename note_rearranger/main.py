dd70efcdbb09dc03286be112c343bb381c66db37
Glutanimate <glutanimate@users.noreply.github.com>
2017-04-14 10:30:52 +0200

    Restructure

--- note_rearranger/main.py
+++ note_rearranger/main.py
@@ -15,143 +15,7 @@ from aqt.browser import Browser
 from anki.hooks import addHook, wrap
 
 from .forms import rearranger
-
-
-class NoteTable(QTableWidget):
-    """Custom QTableWidget that allows dragging rows"""
-    # based on http://stackoverflow.com/a/26311179
-    def __init__(self, dialog):
-        QTableWidget.__init__(self)
-
-        self.dialog = dialog
-        self.setDragEnabled(True)
-        self.setAcceptDrops(True)
-        self.viewport().setAcceptDrops(True)
-        self.setDragDropOverwriteMode(False)
-        self.setDropIndicatorShown(True)
-
-        self.setSelectionMode(QAbstractItemView.ExtendedSelection) 
-        self.setSelectionBehavior(QAbstractItemView.SelectRows)
-        self.setDragDropMode(QAbstractItemView.InternalMove)
-
-        self.setEditTriggers(QAbstractItemView.NoEditTriggers)
-
-        hh = self.horizontalHeader()
-        hh.setStretchLastSection(True)
-        hh.setResizeMode(QHeaderView.Interactive)
-        hh.setCascadingSectionResizes(False)
-
-
-    def dropEvent(self, event):
-        if event.source() == self and (event.dropAction() == Qt.MoveAction 
-                        or self.dragDropMode() == QAbstractItemView.InternalMove):
-            success, row, col, topIndex = self.dropOn(event)
-            if success:             
-                selRows = self.getSelectedRowsFast()                        
-
-                top = selRows[0]
-                dropRow = row
-                if dropRow == -1:
-                    dropRow = self.rowCount()
-                offset = dropRow - top
-
-                for i, row in enumerate(selRows):
-                    r = row + offset
-                    if r > self.rowCount() or r < 0:
-                        r = 0
-                    self.insertRow(r)
-
-                selRows = self.getSelectedRowsFast()
-
-                top = selRows[0]
-                offset = dropRow - top                
-                for i, row in enumerate(selRows):
-                    r = row + offset
-                    if r > self.rowCount() or r < 0:
-                        r = 0
-
-                    for j in range(self.columnCount()):
-                        source = QTableWidgetItem(self.item(row, j))
-                        self.setItem(r, j, source)
-
-                event.accept()
-
-        else:
-            QTableView.dropEvent(event)                
-
-
-    def getSelectedRowsFast(self):
-        selRows = []
-        for item in self.selectedItems():
-            if item.row() not in selRows:
-                selRows.append(item.row())
-        return selRows
-
-
-    def droppingOnItself(self, event, index):
-        dropAction = event.dropAction()
-
-        if self.dragDropMode() == QAbstractItemView.InternalMove:
-            dropAction = Qt.MoveAction
-
-        if event.source() == self and event.possibleActions() & Qt.MoveAction and dropAction == Qt.MoveAction:
-            selectedIndexes = self.selectedIndexes()
-            child = index
-            while child.isValid() and child != self.rootIndex():
-                if child in selectedIndexes:
-                    return True
-                child = child.parent()
-
-        return False
-
-
-    def dropOn(self, event):
-        if event.isAccepted():
-            return False, None, None, None
-
-        index = QModelIndex()
-        row = -1
-        col = -1
-
-        if self.viewport().rect().contains(event.pos()):
-            index = self.indexAt(event.pos())
-            if not index.isValid() or not self.visualRect(index).contains(event.pos()):
-                index = self.rootIndex()
-
-        if self.model().supportedDropActions() & event.dropAction():
-            if index != self.rootIndex():
-                dropIndicatorPosition = self.position(event.pos(), self.visualRect(index), index)
-
-                if dropIndicatorPosition == QAbstractItemView.AboveItem:
-                    row = index.row()
-                    col = index.column()
-                elif dropIndicatorPosition == QAbstractItemView.BelowItem:
-                    row = index.row() + 1
-                    col = index.column()
-                else:
-                    row = index.row()
-                    col = index.column()
-
-            if not self.droppingOnItself(event, index):
-                return True, row, col, index
-
-        return False, None, None, None
-
-
-    def position(self, pos, rect, index):
-        r = QAbstractItemView.OnViewport
-        margin = 2
-        if pos.y() - rect.top() < margin:
-            r = QAbstractItemView.AboveItem
-        elif rect.bottom() - pos.y() < margin:
-            r = QAbstractItemView.BelowItem 
-        elif rect.contains(pos, True):
-            r = QAbstractItemView.OnItem
-
-        if r == QAbstractItemView.OnItem and not (self.model().flags(index) & Qt.ItemIsDropEnabled):
-            r = QAbstractItemView.AboveItem if pos.y() < rect.center().y() else QAbstractItemView.BelowItem
-
-        return r
+from .notetable import NoteTable
 
 
 class RearrangerDialog(QDialog):
@@ -160,11 +24,11 @@ class RearrangerDialog(QDialog):
         super(RearrangerDialog, self).__init__(parent=browser)
         self.browser = browser
         self.mw = browser.mw
-        self.clicked = False
+
         # load qt-designer form:
         self.f = rearranger.Ui_Dialog()
         self.f.setupUi(self)
-        self.table = NoteTable(self, browser)
+        self.table = NoteTable(self)
         self.f.tableLayout.addWidget(self.table)
         self.f.buttonBox.accepted.connect(self.onAccept)
         self.f.buttonBox.rejected.connect(self.onReject)
@@ -185,7 +49,7 @@ class RearrangerDialog(QDialog):
 
 
     def onCellClicked(self, row, col):
-        self.clicked = True
+        """Sync row change to Browser"""
         nid = self.table.item(row, 0).text()
         cids = self.mw.col.db.list(
                 "select id from cards where nid = ? order by ord", nid)
@@ -196,7 +60,6 @@ class RearrangerDialog(QDialog):
                 break
         if cid:
             self.browser.focusCid(cid)
-        self.clicked = False
 
 
     def fillTable(self):
@@ -258,6 +121,10 @@ def onBrowserRowChanged(self, current, previous):
     nid = str(self.card.nid)
     self._rearranger.focusNid(nid)
 
+def onBrowserClose(self, evt):
+    """Close with browser"""
+    if self._rearranger:
+        self._rearranger.close()
 
 def onRearrange(self):
     """Invoke Rearranger window"""
@@ -283,3 +150,4 @@ Browser._rearranger = None
 addHook("browser.setupMenus", setupMenu)
 Browser.onRearrange = onRearrange
 Browser.onRowChanged = wrap(Browser.onRowChanged, onBrowserRowChanged, "after")
+Browser.closeEvent = wrap(Browser.closeEvent, onBrowserClose, "before")
---
+++ note_rearranger/notetable.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+
+"""
+This file is part of the Note Rearranger add-on for Anki
+
+Note table widget
+
+Copyright: Glutanimate 2017
+License: GNU AGPL, version 3 or later; https://www.gnu.org/licenses/agpl-3.0.en.html
+"""
+
+class NoteTable(QTableWidget):
+    """Custom QTableWidget with drag-and-drop support"""
+    # based on http://stackoverflow.com/a/26311179
+    def __init__(self, dialog):
+        QTableWidget.__init__(self)
+
+        self.dialog = dialog
+        self.setDragEnabled(True)
+        self.setAcceptDrops(True)
+        self.viewport().setAcceptDrops(True)
+        self.setDragDropOverwriteMode(False)
+        self.setDropIndicatorShown(True)
+
+        self.setSelectionMode(QAbstractItemView.ExtendedSelection) 
+        self.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.setDragDropMode(QAbstractItemView.InternalMove)
+
+        self.setEditTriggers(QAbstractItemView.NoEditTriggers)
+
+        hh = self.horizontalHeader()
+        hh.setStretchLastSection(True)
+        hh.setResizeMode(QHeaderView.Interactive)
+        hh.setCascadingSectionResizes(False)
+
+
+    def dropEvent(self, event):
+        if event.source() == self and (event.dropAction() == Qt.MoveAction 
+                        or self.dragDropMode() == QAbstractItemView.InternalMove):
+            success, row, col, topIndex = self.dropOn(event)
+            if success:             
+                selRows = self.getSelectedRowsFast()                        
+
+                top = selRows[0]
+                dropRow = row
+                if dropRow == -1:
+                    dropRow = self.rowCount()
+                offset = dropRow - top
+
+                for i, row in enumerate(selRows):
+                    r = row + offset
+                    if r > self.rowCount() or r < 0:
+                        r = 0
+                    self.insertRow(r)
+
+                selRows = self.getSelectedRowsFast()
+
+                top = selRows[0]
+                offset = dropRow - top                
+                for i, row in enumerate(selRows):
+                    r = row + offset
+                    if r > self.rowCount() or r < 0:
+                        r = 0
+
+                    for j in range(self.columnCount()):
+                        source = QTableWidgetItem(self.item(row, j))
+                        self.setItem(r, j, source)
+
+                event.accept()
+
+        else:
+            QTableView.dropEvent(event)                
+
+
+    def getSelectedRowsFast(self):
+        selRows = []
+        for item in self.selectedItems():
+            if item.row() not in selRows:
+                selRows.append(item.row())
+        return selRows
+
+
+    def droppingOnItself(self, event, index):
+        dropAction = event.dropAction()
+
+        if self.dragDropMode() == QAbstractItemView.InternalMove:
+            dropAction = Qt.MoveAction
+
+        if event.source() == self and event.possibleActions() & Qt.MoveAction and dropAction == Qt.MoveAction:
+            selectedIndexes = self.selectedIndexes()
+            child = index
+            while child.isValid() and child != self.rootIndex():
+                if child in selectedIndexes:
+                    return True
+                child = child.parent()
+        return False
+
+
+    def dropOn(self, event):
+        if event.isAccepted():
+            return False, None, None, None
+
+        index = QModelIndex()
+        row = -1
+        col = -1
+
+        if self.viewport().rect().contains(event.pos()):
+            index = self.indexAt(event.pos())
+            if not index.isValid() or not self.visualRect(index).contains(event.pos()):
+                index = self.rootIndex()
+
+        if self.model().supportedDropActions() & event.dropAction():
+            if index != self.rootIndex():
+                dropIndicatorPosition = self.position(event.pos(), self.visualRect(index), index)
+
+                if dropIndicatorPosition == QAbstractItemView.AboveItem:
+                    row = index.row()
+                    col = index.column()
+                elif dropIndicatorPosition == QAbstractItemView.BelowItem:
+                    row = index.row() + 1
+                    col = index.column()
+                else:
+                    row = index.row()
+                    col = index.column()
+
+            if not self.droppingOnItself(event, index):
+                return True, row, col, index
+        return False, None, None, None
+
+
+    def position(self, pos, rect, index):
+        r = QAbstractItemView.OnViewport
+        margin = 2
+        if pos.y() - rect.top() < margin:
+            r = QAbstractItemView.AboveItem
+        elif rect.bottom() - pos.y() < margin:
+            r = QAbstractItemView.BelowItem 
+        elif rect.contains(pos, True):
+            r = QAbstractItemView.OnItem
+
+        if r == QAbstractItemView.OnItem and not (self.model().flags(index) & Qt.ItemIsDropEnabled):
+            r = QAbstractItemView.AboveItem if pos.y() < rect.center().y() else QAbstractItemView.BelowItem
+
+        return r
