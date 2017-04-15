0b01ac0e788b6c1dca852cfc86da8c67d6ca1289
Glutanimate <glutanimate@users.noreply.github.com>
2017-04-15 19:33:46 +0200

    More refactoring

--- note_organizer/rearranger.py
+++ note_organizer/rearranger.py
@@ -11,22 +11,21 @@ License: GNU AGPL, version 3 or later; https://www.gnu.org/licenses/agpl-3.0.en.
 
 from aqt import mw
 
-
 class Rearranger:
 
-    def __init__(self, browser, modified):
+    def __init__(self, browser, moved):
         self.browser = browser
-        self._modified = modified
+        self.moved = moved
 
     def rearrange(self, nids):
+        """Adjust nid order"""
+        modified = []
+        # Full database sync required:
         mw.col.modSchema(check=True)
-        mw.progress.start(label="Note Reorganizer: updating...", max = len(nids))
+        # Create undo checkpoint
         mw.checkpoint("Reorganize notes")
 
-        modified = []
-
         print "\n" * 4
-
         last = nids.pop(0)
         for idx, nid in enumerate(nids):
             
@@ -36,43 +35,42 @@ class Rearranger:
             except IndexError:
                 nxt = nid+1
 
+            # check if order as expected
             if last < nid < nxt:
-                if not any(nid>i for i in nids[idx:]): # block moved
+                # above check won't work if we moved an entire block,
+                # so we need to check against all remaining indices
+                if not any(nid>i for i in nids[idx:]):
                     last = nid
                     continue
         
-            dest = last+1
+            new_nid = last + 1
+            # Ensure timestamp doesn't already exist
+            while mw.col.db.scalar(
+                    "select id from notes where id = ?", new_nid):
+                new_nid += 1
+
             print "=================================="
             print "last", last
             print "nid", nid
             print "next", nxt
+            print "new_nid", new_nid
 
-            # Ensure timestamp doesn't already exist
-            while mw.col.db.scalar(
-                    "select id from notes where id = ?", dest):
-                dest += 1
-
-            print "dest", dest
-
-            # Update the note row
+            # Update note row
             mw.col.db.execute(
-                """update notes set id=? where id = ?""", dest, nid)
+                """update notes set id=? where id = ?""", new_nid, nid)
 
-            # Update the cards row(s)
+            # Update cards rows
             mw.col.db.execute(
-                """update cards set nid=? where nid = ?""", dest, nid)
+                """update cards set nid=? where nid = ?""", new_nid, nid)
 
-            last = dest
-            if nid in self._modified:
-                modified.append(dest)
+            last = new_nid
 
-        mw.progress.finish()
+            if nid in self.moved:
+                modified.append(new_nid)
 
         mw.reset()
-        
-        #self.selectNotes(modified)
+        self.selectNotes(modified)
 
-        return
 
     def selectNotes(self, nids):
         sm = self.browser.form.tableView.selectionModel()
